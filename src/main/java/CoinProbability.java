b3bc50e56fbb6028eb40c62827eb72a19f42e977
Veda Varshitha <veda.varshitha18@gmail.com>
2021-04-11 21:59:06 +0530

    IMplemented probability of two events occuring together

--- src/main/java/CoinProbability.java
+++ src/main/java/CoinProbability.java
@@ -7,6 +7,18 @@ public class CoinProbability {
         this.tailProbability = tailProbability;
     }
 
+    public static double and(double event1_Probability, double event2_Probability) {
+        return event1_Probability*event2_Probability;
+    }
+
+    public double getHeadProbability() {
+        return headProbability;
+    }
+
+    public double getTailProbability() {
+        return tailProbability;
+    }
+
     @Override
     public boolean equals(Object obj) {
         CoinProbability coinProbability =(CoinProbability)obj;
---
+++ src/test/java/CoinProbabilityTest.java
@@ -0,0 +1,33 @@
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+public class CoinProbabilityTest {
+
+    @Test
+    public void testForHeadsProbabilityEqualToTailsProbability(){
+        double headsProbability = 0.5;
+        double tailsProbability = 0.5;
+
+        CoinProbability coinProbability = new CoinProbability(headsProbability, tailsProbability);
+
+        assertEquals(true, coinProbability.equals(coinProbability));
+    }
+
+    @Test
+    public void testForOneHeadAndOneTailOccurringTogether() {
+        double headsProbability = 0.5;
+        double tailsProbability = 0.5;
+        double expectedValue = 0.25;
+        double actualValue;
+        CoinProbability coin1 = new CoinProbability(headsProbability, tailsProbability);
+        CoinProbability coin2 = new CoinProbability(headsProbability, tailsProbability);
+        double coin1_HeadProbability = coin1.getHeadProbability();
+        double coin2_TailProbability = coin2.getTailProbability();
+
+        actualValue = CoinProbability.and(coin1_HeadProbability, coin2_TailProbability);
+
+        assertEquals(expectedValue, actualValue, 0.0001);
+    }
+}
